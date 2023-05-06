7279b66acccb7121910a0132c73c923ebf384197
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-06 22:58:27 +0200

    Remove Item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -38,8 +38,11 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
+
     const existingItem = state.items[existingCartItemIndex];
+
     const updatedTotalAmount = state.totalAmount - existingItem.price;
+
     let updatedItems;
     if (existingItem.amount === 1) {
       updatedItems = state.items.filter((item) => item.id !== action.id);
